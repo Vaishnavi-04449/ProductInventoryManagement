821af23153888062fadc235a09405f9ac18247f9
Vaishnavi-04449 <vaishnavias204@gmail.com>
2026-03-23 20:36:17 +0530

    Add files via upload

--- Data/ProductDbcontext.cs
+++ Data/ProductDbcontext.cs
@@ -1,1 +1,13 @@
-
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventoryAPI.Models;
+namespace ProductInventoryAPI.Data
+{
+    public class ProductDbContext:DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            :base(options)
+        {
+        }
+        public DbSet<Product> Products{get;set;}
+    }
+}
